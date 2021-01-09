8e98962c8710d8901f786687a1df0cfe31abf0ea
Henrique Pantaroto <76695749+HenriquePantaroto@users.noreply.github.com>
2021-01-09 10:08:24 -0300

    Jogo de Jokenpô

---
+++ Jogos em Python/Jokenpô.py
@@ -0,0 +1,47 @@
+import random
+from time import sleep
+
+print('\n\033[36m-=-=-=-=-=-Seja bem vindo ao desafio JOKÊNPO-=-=-=-=-=-\033[m\n')
+sleep(2)
+print('\033[36mEu sou seu desafiante, quero ver você me vencer!\033[m\n')
+sleep(2)
+print('\033[36mO desafio é simples, basta você escrever qual das opções você irá lançar.\033[m\n')
+sleep(2)
+print('\033[36mLembrando que:\033[m\n')
+sleep(2)
+print('\033[31mPapel > Pedra\033[m')
+sleep(2)
+print('\033[31mPedra > Tesoura\033[m')
+sleep(2)
+print('\033[31mTesoura > Papel\033[m')
+sleep(2)
+
+print('\n\033[36mEstá pronto?\033[m\n')
+
+lista = ['pedra', 'papel', 'tesoura']
+computador = random.choice(lista)
+usuario = input('Faça sua escolha (Pedra, Papel, Tesoura): ').lower()
+print('Eu escolhi {}.'.format(computador))
+
+while True:
+    if computador == usuario:
+        print('Nós empatamos, vamos de novo.')
+        usuario = input('Faça sua escolha (Pedra, Papel, Tesoura): ').lower()
+    else:
+        break
+
+if computador == 'pedra' and usuario == 'papel':
+    print('Você me venceu.')
+elif computador == 'pedra' and usuario == 'tesoura':
+    print('Eu ganhei de você.')
+elif computador == 'papel' and usuario == 'pedra':
+    print('Eu ganhei de você.')
+elif computador == 'papel' and usuario == 'tesoura':
+    print('Você me venceu.')
+elif computador == 'tesoura' and usuario == 'papel':
+    print('Eu ganhei de você.')
+elif computador == 'tesoura' and usuario == 'pedra':
+    print('Você me venceu.')
+else:
+    print('Palavra não identificada, tente novamente.')
+
